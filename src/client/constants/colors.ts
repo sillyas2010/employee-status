ca85c62b11d93e449786e59537f442e01b326a85
sillyas2010 <sillyas2010@gmail.com>
2024-11-29 20:13:13 +0200

    change colors to match design

--- src/client/constants/colors.ts
+++ src/client/constants/colors.ts
@@ -1,16 +1,16 @@
-import tailwindColors from 'tailwindcss/colors';
+import tailwindColors from 'tailwindcss/colors'
 
-const primaryPalette = { ...tailwindColors.blue, 500: "#479AEA" }
+const primaryPalette = { ...tailwindColors.blue, 500: '#479AEA' }
 const colors = {
-  primary: {
-    ...primaryPalette,
-    DEFAULT: primaryPalette[500],
-  },
-  secondary: {
-    ...tailwindColors.neutral,
-    DEFAULT: tailwindColors.neutral[200],
-  },
-  background: tailwindColors.neutral[100],
+	primary: {
+		...primaryPalette,
+		DEFAULT: primaryPalette[500],
+	},
+	secondary: {
+		...tailwindColors.neutral,
+		DEFAULT: tailwindColors.neutral[200],
+	},
+	background: tailwindColors.neutral[100],
 }
 
 export default colors
